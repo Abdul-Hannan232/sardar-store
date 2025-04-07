123baa592e7aba740e3dcf9d301c1ec25c1dde01
Abdul-Hannan232 <abdulhani232@gmail.com>
2025-04-07 21:59:06 +0500

    resolve build issue ---- async home page banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,36 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const { data:{banners} } = await http.get(
-    `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/banners`,
-  );
+  // const { data:{banners} } = await http.get(
+  //   `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/banners`,
+  // );
 
-  const today = new Date(); 
-  const visibleBanners = banners.filter((banner:any) => banner.isVisible && new Date(banner.endingDate) >= today);
+  // const today = new Date(); 
+  // const visibleBanners = banners.filter((banner:any) => banner.isVisible && new Date(banner.endingDate) >= today);
+
+
+  let banners = [];
+
+  try {
+    const res = await http.get(
+      `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/banners`
+    );
+    const today = new Date();
+    banners = res.data.banners.filter(
+      (banner: any) =>
+        banner.isVisible && new Date(banner.endingDate) >= today
+    );
+  } catch (error) {
+    console.error("Failed to fetch banners", error);
+    banners = [];
+  }
 
   return (
     <>
     
       <Header />
       <Container className="mt-24">
-        <HeroCarouselBlock heroBanner={visibleBanners} />
+        <HeroCarouselBlock heroBanner={banners} />
         {/* <HeroCarouselBlock heroBanner={heroBanner} /> */}
         <CategoryWithProduct />
       </Container>
