0ecfc97d6064927cacb4f251168e779fc994fee2
Abdul-Hannan232 <abdulhani232@gmail.com>
2025-05-29 15:48:09 +0500

    set categories popup close button on top

--- src/components/category/category-popup.tsx
+++ src/components/category/category-popup.tsx
@@ -4,9 +4,16 @@ import { useModalAction } from '@components/common/modal/modal.context';
 const CategoryPopup = () => {
   const { closeModal } = useModalAction();
   return (
-    <div className="relative flex flex-col w-full mx-auto overflow-hidden rounded-t bg-brand-light h-inherit">
+    <div className="relative flex flex-col w-full mx-auto overflow-hidden rounded-t  bg-brand-light h-inherit">
+       <button
+          onClick={closeModal}
+          aria-label="Close Modal"
+          className="w-full bg-fill-base tracking-[0.025em] rounded-md transition text-brand-dark duration-200 h-14 text-center font-semibold text-sm block hover:bg-brand hover:text-brand-light focus:outline-none"
+        >
+         Close
+        </button>
       <CategoryDropdownSidebar className="w-full" />
-      <div className="absolute bottom-0 w-full p-4 text-brand-light shadow-card shrink-0 bg-brand-light">
+      {/* <div className="absolute bottom-0 w-full p-4 text-brand-light shadow-card shrink-0 bg-brand-light">
         <button
           onClick={closeModal}
           aria-label="Close Modal"
@@ -14,7 +21,7 @@ const CategoryPopup = () => {
         >
           Close
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
